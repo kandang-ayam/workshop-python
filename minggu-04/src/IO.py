c41a89037bb4577bc6e161e3c2d5d2534311a896
kandang-ayam <71696078+kandang-ayam@users.noreply.github.com>
2021-05-01 21:54:43 +0700

    Update IO.py

--- minggu-04/src/IO.py
+++ minggu-04/src/IO.py
@@ -14,3 +14,13 @@ Please enter an integer: 7
 More
 >>>
 
+
+>>> # Measure some strings:
+>>> words = ['cat', 'window', 'defenestrate']
+>>> for w in words:
+...     print(w, len(w))
+...
+cat 3
+window 6
+defenestrate 12
+>>>
